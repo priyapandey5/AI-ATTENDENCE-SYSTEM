e9385eebef09bcec628f10abd3166c44f244796f
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-05-26 12:09:02 +0530

    Update base_layout.py

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -10,7 +10,7 @@ def style_background_home():
                 }
 
                 .stApp div[data-testid="stColumn"]{
-                    background-color:#E0E3FF !important;
+                    background-color:#313340 !important;
                     font-color:#0e0f14;
                     padding:2.5rem !important;
                     border-radius: 5rem !important;
@@ -27,7 +27,7 @@ def style_background_dashboard():
         <style>
 
                 .stApp {
-                    background: #E0E3FF !important;
+                    background: #313340 !important;
                 }
 
         </style>  
