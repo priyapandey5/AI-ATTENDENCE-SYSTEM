8195f76e1254e3fcf1a22bf1cb37487ee19ceb04
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-05-26 11:59:57 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,8 +6,8 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        <p style="font-weight:bold; color:white;"> Created with ❤️ </p>  
+        
         </div>
                 
                 """, unsafe_allow_html=True)
@@ -19,7 +19,7 @@ def footer_dashboard():
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style=="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        
         </div>
                 
                 """, unsafe_allow_html=True)
