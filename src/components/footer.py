ccc93e4b3069d65b4b32a434f59b7d42cfdb6ed7
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2026-05-26 12:22:38 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -18,7 +18,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style=="font-weight:bold; color:black;"> Created with ❤️ by </p>  
+        <p style=="font-weight:bold; color:black;"> Created with ❤️</p>  
         
         </div>
                 
